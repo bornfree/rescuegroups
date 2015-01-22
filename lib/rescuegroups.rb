1a34e45b0ae513ce4560177693b0f5fc7dde4884
Chris D'Ambrosio <cdambrosio@gmail.com>
2015-01-22 14:17:48 -0800

    Fix dependencies
    - Include activesupport (Client uses Array#present?)
    - The json and curb gems are also required during runtime

--- lib/rescuegroups.rb
+++ lib/rescuegroups.rb
@@ -1,5 +1,7 @@
 require "json"
 require "curb"
+require "active_support/core_ext/array"
+
 require "rescuegroups/version"
 require "rescuegroups/configuration"
 require "rescuegroups/client"
@@ -7,4 +9,3 @@ require "rescuegroups/client"
 module Rescuegroups
   extend Configuration
 end
-
